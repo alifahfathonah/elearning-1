41aaa4a73ffd9e3d7b45f8dd7fa34827b5772382
thehananbhat <bhathanan66@gmail.com>
2020-07-31 12:32:15 +0530

    Added codebase for elearning platform

---
+++ footer.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+</head>
+<body>
+
+	<footer id="footer">
+		<div class="container">
+			<div class="social text-center">
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-dribbble"></i></a>
+				<a href="#"><i class="fa fa-flickr"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+			</div>
+
+			<div class="clear"></div>
+			<!--CLEAR FLOATS-->
+		</div>
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="index.php">Home</a> | 
+								<a href="login.php">Login</a> | 
+								<a href="login.php">Courses</a> |
+								<a href="contact.php">Feedback</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
